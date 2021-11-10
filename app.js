6a24f2e45b85f43351eabfcbc149ea501d696060
KAMIL <biero.kamil@gmail.com>
2021-11-10 17:37:33 +0100

    Repaired crashing if no internet access

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // Load modules
-const express = require('express');
-const path = require('path');
-const partRoutes = require('./partRoutes');
-
+const express = require("express");
+const path = require("path");
+const partRoutes = require("./partRoutes");
 
 // Create express application
 const app = express();
@@ -15,9 +14,9 @@ app.use(express.json());
 
 // Listen on port 3000 for connections
 app.listen(3000, () => {
-  console.log('Server started and listening at http://localhost:3000');
+  console.log("Server started and listening at http://localhost:3000");
 });
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(partRoutes);
--- controllers/partControllers.js
+++ controllers/partControllers.js
@@ -1,41 +1,43 @@
 // Load modules
-const partModel = require('../models/partModel');
+const partModel = require("../models/partModel");
 let Request = require("request");
-const unirest = require('unirest');
+const unirest = require("unirest");
 
-function search_part_get (request, response) {
-  unirest.get("https://api.nbp.pl/api/exchangerates/rates/a/eur/")
-  .end(function (result) {
-    
-    let euroPrice = result.body.rates[0].mid;
-    response.render('index', {euroPrice: euroPrice});
-    console.log( euroPrice);
-  });
-};
+function search_part_get(request, response) {
+  unirest
+    .get("https://api.nbp.pl/api/exchangerates/rates/a/eur/")
+    .end(function (result) {
+      let euroPrice = "brak internetu";
+      if (result.body != undefined) euroPrice = result.body.rates[0].mid;
+
+      response.render("index", { euroPrice: euroPrice });
+      console.log(euroPrice);
+    });
+}
 // Index page controller
 
 // POST
-function search_part_post (request, response) {
+function search_part_post(request, response) {
   const part_number = request.body.Partnumber;
   console.log(request.body.Partnumber);
-  unirest.get("https://api.nbp.pl/api/exchangerates/rates/a/eur/")
-  .end(function (result) {
-    
-    let euroPrice = result.body.rates[0].mid;
-    // response.render('index', {euroPrice: euroPrice});
-    // console.log( euroPrice);
+  unirest
+    .get("https://api.nbp.pl/api/exchangerates/rates/a/eur/")
+    .end(function (result) {
+      let euroPrice = "brak internetu";
+      if (result.body != undefined) euroPrice = result.body.rates[0].mid;
 
-    partModel.getPartInformation(part_number, (result) => {
-      console.log(result);
-      response.render('index', { searchPart: result, euroPrice: euroPrice });
-   
+      // response.render('index', {euroPrice: euroPrice});
+      // console.log( euroPrice);
+
+      partModel.getPartInformation(part_number, (result) => {
+        console.log(result);
+        response.render("index", { searchPart: result, euroPrice: euroPrice });
+      });
     });
-
-  });
-};
+}
 
 // Export controllers
 module.exports = {
   search_part_post,
-  search_part_get
+  search_part_get,
 };
